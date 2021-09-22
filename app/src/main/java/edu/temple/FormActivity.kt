343b47e214658659cdcaf64633bc3e25f52c49da
doingthisforclass <zam0400@gmail.com>
2021-09-22 13:49:20 -0400

    logic added

--- app/src/main/java/edu/temple/FormActivity.kt
+++ app/src/main/java/edu/temple/FormActivity.kt
@@ -11,11 +11,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var user_name = findViewById(R.id.user_name) as EditText
-        var password = findViewById(R.id.user_password) as EditText
-        var email_Address = findViewById(R.id.email_Address) as EditText
-        var password_Confirm = findViewById(R.id.password_Confirm) as EditText
-        var btn_submit = findViewById(R.id.btn_submit) as Button
+        val user_name = findViewById<EditText>(R.id.user_name)
+        val password = findViewById<EditText>(R.id.user_password)
+        val email_Address = findViewById<EditText>(R.id.email_Address)
+        val password_Confirm = findViewById<EditText>(R.id.password_Confirm)
+        val btn_submit = findViewById<Button>(R.id.btn_submit)
 
      //   val email = findViewById<EditText>(R.id.email)
      //   val passwordConfirm = findViewById<EditText>(R.id.passwordConfirm) as EditText
@@ -25,31 +25,38 @@ class MainActivity : AppCompatActivity() {
 
         // set on-click listener
         btn_submit.setOnClickListener {
-            val user_name = user_name.text;
-            val password = password.text;
-            val email_Address = email_Address.text;
-            val password_Confirm = password_Confirm.text;
+            val user_name = user_name.text
+            val password = password.text
+            val email_Address = email_Address.text
+            val password_Confirm = password_Confirm.text
 
-            if (user_name.isEmpty()){
-                Toast.makeText(this, "Please enter a name.", Toast.LENGTH_LONG).show()
-            }else if( password.isEmpty()) {
-                Toast.makeText(this, "Please enter a password.", Toast.LENGTH_LONG).show()
-            }else if( email_Address.isEmpty()) {
-                Toast.makeText(this, "Please enter a email Address.", Toast.LENGTH_LONG).show()
-            }else if( password_Confirm.isEmpty()) {
-                Toast.makeText(this, "Please confirm your password.", Toast.LENGTH_LONG).show()
-            }else{
-                Toast.makeText(this, "Finished", Toast.LENGTH_LONG).show()
+
+
+            when {
+                user_name.isEmpty() -> {
+                    Toast.makeText(this, "Please enter a name.", Toast.LENGTH_LONG).show()
+                }
+                password.isEmpty() -> {
+                    Toast.makeText(this, "Please enter a password.", Toast.LENGTH_LONG).show()
+                }
+                email_Address.isEmpty() -> {
+                    Toast.makeText(this, "Please enter a email Address.", Toast.LENGTH_LONG).show()
+                }
+                password_Confirm.isEmpty() -> {
+                    Toast.makeText(this, "Please confirm your password.", Toast.LENGTH_LONG).show()
+                }
+                password.toString() == password_Confirm.toString() -> {
+                    Toast.makeText(this, "Password confirmed", Toast.LENGTH_LONG).show()
+                    Toast.makeText(this, "Welcome: $user_name", Toast.LENGTH_LONG).show()
+                }
+                else -> {
+                    Toast.makeText(this, "Passwords do not match", Toast.LENGTH_LONG).show()
+                    println(password)
+                    println(password_Confirm)
+                }
             }
-
-
-            // your code to validate the user_name and password combination
-            // and verify the same
-
+            }
         }
-
-
-    }
 
 
 }
